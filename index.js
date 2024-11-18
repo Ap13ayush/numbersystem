9cd2aed0e719b3098bf064a0f68fae85e4fe177d
Ayush Patel <111946055+Ap13ayush@users.noreply.github.com>
2024-11-18 16:59:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// writing code for an even or odd number;
+const numbers = [3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    console.log("even" + numbers[i]);
+  } else {
+    console.log("odd" + numbers[i]);
+  }
+}
+
